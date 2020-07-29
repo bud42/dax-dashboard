d7cec65b3d523bb5851017697c07c22438ef1f8e
bud42 <bdboyd42@gmail.com>
2020-07-29 09:56:03 -0400

    bugfix

--- dashboard/jobreport.py
+++ dashboard/jobreport.py
@@ -337,7 +337,8 @@ class DaxDashboard:
 
             # Make a 1x1 figure (I dunno why, this is from doing multi plots)
             #fig = plotly.subplots.make_subplots(rows=1, cols=1)
-            fig = go.Figure(xaxis={'showgrid': False, 'zeroline': False})
+            #fig = go.Figure(xaxis={'showgrid': False, 'zeroline': False})
+            fig = go.Figure()
 
             # What index are we pivoting on to count statuses
             PINDEX = selected_groupby
