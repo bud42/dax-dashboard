78fccafed5693e7e08a45c559f6f9ed014d55ab9
bud42 <bdboyd42@gmail.com>
2026-05-21 18:23:17 -0400

    updates

--- launcher.py
+++ launcher.py
@@ -8,6 +8,7 @@ from daxdashboard.login import app
 from daxdashboard.serv import ServerThread
 
 
+print("START")
 print(sys.executable)
 print(sys.frozen if hasattr(sys, "frozen") else "not frozen")
 
